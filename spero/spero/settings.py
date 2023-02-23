644f30561c23f5e02d008a7ccff66c60e080cf1a
Shreyas Chikhale <79449913+shreyas0206@users.noreply.github.com>
2023-02-23 20:39:05 +0530

    Update settings.py

--- spero/spero/settings.py
+++ spero/spero/settings.py
@@ -79,7 +79,7 @@ DATABASES =  {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'sperodb',  
         'USER':'root',  
-        'PASSWORD':'1234',  
+        'PASSWORD':'',  
         'HOST':'localhost',  
         'PORT':'3306'  
     }  
